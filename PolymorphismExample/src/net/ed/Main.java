1e6a300eda2066c20d91b72122359e1fabaaafd5
Edward Dupre <edwd42@gmail.com>
2018-01-09 22:32:39 -0500

    add comment to main() to describe the purpose of the program

--- PolymorphismExample/src/net/ed/Main.java
+++ PolymorphismExample/src/net/ed/Main.java
@@ -1,6 +1,13 @@
 package net.ed;
 
-
+/**
+ * Assignment: Describe an OOP Concept
+ * Pick one of the OOP concepts: Inheritance, Encapsulation, Polymorphism etc... and write up one paragraph on a google document.
+ *
+ * For this assignment, I chose to describe Polymorphism because it’s not easy to explain.  Also, I like the way the Lego video explained it in Section 4 Lecture 9 in this video →   https://www.udemy.com/master-object-orientation-with-lego-illustrations/learn/v4/t/lecture/4038000?start=0
+ *
+ * Basically, polymorphism allows the program to do one thing for one type of object, and do a different thing for a different type of object.
+ */
 
 public class Main {
 
