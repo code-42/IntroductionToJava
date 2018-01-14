43c6c708fc435f63f9b0d07cd2503ee195ac2bef
Edward Dupre <edwd42@gmail.com>
2018-01-14 18:08:06 -0500

    add JUnit testing framework to project

---
+++ CompositionExample/tests/net/ed/AccountTest.java
@@ -0,0 +1,29 @@
+package net.ed;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AccountTest {
+
+    CheckingAccount checking = new CheckingAccount("Ed", 100);
+    SavingAccount saving  = new SavingAccount("Ed", 200);
+    Customer customer1 = new Customer(checking, saving);
+
+    @Test
+    public void deposit() {
+        checking.deposit(100.00);
+    }
+
+    @Test
+    public void withdraw() {
+    }
+
+    @Test
+    public void getAccountHolder() {
+    }
+
+    @Test
+    public void getBalance() {
+    }
+}
