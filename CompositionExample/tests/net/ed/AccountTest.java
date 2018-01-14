5d6c0cf9ba7e370ee20c62a81b3ed94754a4eccb
Edward Dupre <edwd42@gmail.com>
2018-01-14 18:30:19 -0500

    edited AccountTest git annotations

--- CompositionExample/tests/net/ed/AccountTest.java
+++ CompositionExample/tests/net/ed/AccountTest.java
@@ -6,18 +6,12 @@ import static org.junit.Assert.*;
 
 public class AccountTest {
 
-<<<<<<< HEAD
     CheckingAccount checking = new CheckingAccount("Ed", 100);
     SavingAccount saving  = new SavingAccount("Ed", 200);
     Customer customer1 = new Customer(checking, saving);
 
     @Test
     public void deposit() {
-        checking.deposit(100.00);
-=======
-    @Test
-    public void deposit() {
->>>>>>> composition_example
     }
 
     @Test
