8362e15e3dfcca2339b7eadbab303366329bb7fe
Edward Dupre <edwd42@gmail.com>
2018-01-14 18:24:33 -0500

    add JUnit testing framework

---
+++ CompositionExample/tests/net/ed/AccountTest.java
@@ -0,0 +1,24 @@
+package net.ed;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AccountTest {
+
+    @Test
+    public void deposit() {
+    }
+
+    @Test
+    public void withdraw() {
+    }
+
+    @Test
+    public void getAccountHolder() {
+    }
+
+    @Test
+    public void getBalance() {
+    }
+}
