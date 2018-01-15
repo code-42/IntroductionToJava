a25936115e4e41458743390e5c3b45cd881a4bf7
Edward Dupre <edwd42@gmail.com>
2018-01-14 20:08:33 -0500

    add ArrayList to hold additional items like cheese, bacon, lettuce, etc

--- OOPChallenge/src/net/ed/Hamburger.java
+++ OOPChallenge/src/net/ed/Hamburger.java
@@ -1,11 +1,16 @@
 package net.ed;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hamburger {
 
     private String name;
     private String meat;
     private String bread;
     private double price;
+
+    List<String> additionalItems = new ArrayList<String>();
 
     // The constructor should only include the roll type, meat and price.
     public Hamburger(String meat, String bread, double price) {
@@ -28,6 +33,12 @@ public class Hamburger {
 
     public double getPrice() {
         return price;
+    }
+
+    public void addItems(String item, double price){
+        this.additionalItems.add(item);
+        this.price += price;
+
     }
 
     public String getOrder(){
