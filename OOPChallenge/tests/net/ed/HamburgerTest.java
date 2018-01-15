ab8de809a0d3128632cf7f2eb187f70316931e26
Edward Dupre <edwd42@gmail.com>
2018-01-14 19:21:09 -0500

    add test for getOrder() method

--- OOPChallenge/tests/net/ed/HamburgerTest.java
+++ OOPChallenge/tests/net/ed/HamburgerTest.java
@@ -1,7 +1,17 @@
 package net.ed;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class HamburgerTest {
 
+    Hamburger basicBurger = new Hamburger("Basic Burger", "ham", "wheat", 5.00);
+
+    @Test
+    public void getOrder() {
+        String expected = "Basic Burger ham wheat 5.00";
+        String actual = basicBurger.getOrder();
+        assertEquals(expected, actual);
+    }
 }
