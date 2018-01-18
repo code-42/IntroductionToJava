bfa566d2d724b3d3e3cfaf14a8a1a8785233116c
Edward Dupre <edwd42@gmail.com>
2018-01-18 15:59:26 -0500

    add class HealthyBurger with fields and constructor and methods addHealthyAddition()

---
+++ HamburgerChallenge/src/net/ed/HealthyBurger.java
@@ -0,0 +1,27 @@
+package net.ed;
+
+public class HealthyBurger extends Hamburger {
+    
+    private String healthyExtra1Name;
+    private double healthyExtra1Price;
+
+    private String healthyExtra2Name;
+    private double healthyExtra2Price;
+
+    public HealthyBurger(String meat, double price) {
+        super("Healthy", meat, "Brown rye", price);
+    }
+
+    public void addHealthAddition1(String name, double price) {
+        this.healthyExtra1Name = name;
+        this.healthyExtra1Price = price;
+    }
+
+    public void addHealthAddition2(String name, double price) {
+        this.healthyExtra2Name = name;
+        this.healthyExtra2Price = price;
+    }
+
+
+
+}
