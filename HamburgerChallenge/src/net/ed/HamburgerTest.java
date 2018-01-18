b78d8cc1ac299c99169066a420d0bf49479cfe97
Edward Dupre <edwd42@gmail.com>
2018-01-18 14:28:13 -0500

    add JUnit test framework

---
+++ HamburgerChallenge/src/net/ed/HamburgerTest.java
@@ -0,0 +1,22 @@
+package net.ed;
+
+import static org.junit.Assert.*;
+
+public class HamburgerTest {
+
+    @org.junit.Test
+    public void getName() {
+    }
+
+    @org.junit.Test
+    public void getMeat() {
+    }
+
+    @org.junit.Test
+    public void getBread() {
+    }
+
+    @org.junit.Test
+    public void getPrice() {
+    }
+}
