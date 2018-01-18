dbb3956859cf05a0b25340185d83b5f8dcc8061f
Edward Dupre <edwd42@gmail.com>
2018-01-18 14:11:55 -0500

    add fields and methods for 4 additionalItems to Hamburger class

--- HamburgerChallenge/src/net/ed/Hamburger.java
+++ HamburgerChallenge/src/net/ed/Hamburger.java
@@ -7,6 +7,19 @@ public class Hamburger {
     private String bread;
     private double price;
 
+    // add additional items not in constructor
+    private String additionalItem1Name;
+    private double additionalItem1Price;
+
+    private String additionalItem2Name;
+    private double additionalItem2Price;
+
+    private String additionalItem3Name;
+    private double additionalItem3Price;
+
+    private String additionalItem4Name;
+    private double additionalItem4Price;
+
     public Hamburger(String name, String meat, String bread, double price) {
         this.name = name;
         this.meat = meat;
@@ -39,4 +52,23 @@ public class Hamburger {
                 ", price=" + price +
                 '}';
     }
+
+    public void addHamburgerAddition1(String name, double price) {
+        this.additionalItem1Name = name;
+        this.additionalItem1Price = price;
+    }
+
+    public void addHamburgerAddition2(String name, double price) {
+        this.additionalItem2Name = name;
+        this.additionalItem2Price = price;
+    }
+    public void addHamburgerAddition3(String name, double price) {
+        this.additionalItem3Name = name;
+        this.additionalItem3Price = price;
+    }
+    public void addHamburgerAddition4(String name, double price) {
+        this.additionalItem4Name = name;
+        this.additionalItem4Price = price;
+    }
+
 }
