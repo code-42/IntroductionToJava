4528953a85ce3493cb1576f3b3d00406b71c004c
Edward Dupre <edwd42@gmail.com>
2018-01-18 13:51:20 -0500

    add test for Hamburger.getName() method

--- HamburgerChallenge/tests/net/ed/HamburgerTest.java
+++ HamburgerChallenge/tests/net/ed/HamburgerTest.java
@@ -6,7 +6,12 @@ import static org.junit.Assert.*;
 
 public class HamburgerTest {
 
+    Hamburger basicBurger = new Hamburger("Basic Burger", "ham", "Regular white bun", 5.55);
+
     @Test
     public void Hamburger() {
+        String expected = "Basic Burger";
+        String actual = basicBurger.getName();
+        assertEquals(expected, actual);
     }
 }
