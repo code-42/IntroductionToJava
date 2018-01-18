f744d6e84c61c50f0be7e76b7bb1c9effae78fe4
Edward Dupre <edwd42@gmail.com>
2018-01-18 13:36:27 -0500

    add Hamburger class with fields and constructor

---
+++ HamburgerChallenge/src/net/ed/Hamburger.java
@@ -0,0 +1,16 @@
+package net.ed;
+
+public class Hamburger {
+
+    private String name;
+    private String meat;
+    private String bread;
+    private double price;
+
+    public Hamburger(String name, String meat, String bread, double price) {
+        this.name = name;
+        this.meat = meat;
+        this.bread = bread;
+        this.price = price;
+    }
+}
