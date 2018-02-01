fb2eac64226f33936f644ff2fbb882c225a594f4
Edward Dupre <edwd42@gmail.com>
2018-02-01 13:14:25 -0500

    remove Main() and create package .jar file

--- PackageChallenge/src/net/ed/Main.java
+++ PackageChallenge/src/net/ed/Main.java
@@ -32,22 +32,4 @@ import net.ed.mylibrary.Series;
 
 public class Main {
 
-    public static void main(String[] args) {
-
-        for(int i=0; i <=10; i++) {
-            System.out.println("nSum(" + i + ") == " + Series.nSum(i));
-        }
-
-        System.out.println("\n*****************************\n");
-        for(int i=0; i<=10; i++) {
-            System.out.println("factorial(" + i + ") == " + Series.factorial(i));
-        }
-
-        System.out.println("\n*****************************\n");
-        for(int i=0; i<=10; i++) {
-            System.out.println("fibonacci(" + i + ") == " + Series.fibonacci(i));
-        }
-
-
-    }
 }
