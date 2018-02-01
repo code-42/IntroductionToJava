84a58a9302dc189345e601260b907e8a7b4e2831
Edward Dupre <edwd42@gmail.com>
2018-02-01 13:18:16 -0500

    create new project and import PackageChallenge.jar file

---
+++ PackageChllengeTest/src/net/ed/packagechallenge/test/Main.java
@@ -0,0 +1,15 @@
+package net.ed.packagechallenge.test;
+
+import net.ed.mylibrary.Series;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for(int i=0; i <=10; i++) {
+            System.out.println("nSum(" + i + ") == " + Series.nSum(i));
+        }
+
+
+    }
+}
