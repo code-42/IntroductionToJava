1087f7a4bac157f688aebf0a0d4830cef7adc4d4
Edward Dupre <edwd42@gmail.com>
2018-02-01 15:09:52 -0500

    initial commit

---
+++ ImmutableClasses/src/net/ed/Main.java
@@ -0,0 +1,15 @@
+package net.ed;
+
+/**
+ * Section 13 Lecture 108 Immutable Class Challenge
+ *
+ * The challenge is to work out what is wrong with the location constructor
+ * on line 14 that would allow the program to compile, and crash at runtime.
+ * When you identify the problem, modify the code to fix it.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
