8541658e505d64bf27d32e177078b942887b63c7
Edward Dupre <edwd42@gmail.com>
2018-02-01 13:28:55 -0500

    call methods from PackageChallenge.jar in main() and they work!

--- PackageChllengeTest/src/net/ed/packagechallenge/test/Main.java
+++ PackageChllengeTest/src/net/ed/packagechallenge/test/Main.java
@@ -9,7 +9,18 @@ public class Main {
         for(int i=0; i <=10; i++) {
             System.out.println("nSum(" + i + ") == " + Series.nSum(i));
         }
+        
+        System.out.println("\n*****************************\n");
 
+        for(int i=0; i<=10; i++) {
+            System.out.println("factorial(" + i + ") == " + Series.factorial(i));
+        }
+
+        System.out.println("\n*****************************\n");
+
+        for(int i=0; i<=10; i++) {
+            System.out.println("fibonacci(" + i + ") == " + Series.fibonacci(i));
+        }
 
     }
 }
