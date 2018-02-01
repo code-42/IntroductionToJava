d9bb113dd2d188eadda563d99d4d75cf0777c91d
Edward Dupre <edwd42@gmail.com>
2018-02-01 13:26:10 -0500

    commit before switch branch

--- PackageChllengeTest/src/net/ed/packagechallenge/test/Main.java
+++ PackageChllengeTest/src/net/ed/packagechallenge/test/Main.java
@@ -6,10 +6,5 @@ public class Main {
 
     public static void main(String[] args) {
 
-        for(int i=0; i <=10; i++) {
-            System.out.println("nSum(" + i + ") == " + Series.nSum(i));
-        }
-
-
     }
 }
