8c0238db9b68d5a9eae4cac19039184513cd8522
Edward Dupre <edwd42@gmail.com>
2018-02-02 21:03:30 -0500

    add vocabulary HashMap

--- AdventureGame/src/net/ed/Main.java
+++ AdventureGame/src/net/ed/Main.java
@@ -19,8 +19,6 @@ import java.util.Scanner;
 
 public class Main {
 
-//    public static void main(String[] args) {
-
         private static HashMap<Integer, Location> locations = new HashMap<Integer, Location>();
 
         public static void main(String[] args) {
@@ -33,6 +31,28 @@ public class Main {
             locations.put(4, new Location(4, "You are in a valley beside a stream"));
             locations.put(5, new Location(5, "You are in the forest"));
 
+            locations.get(1).addExit("W", 2);
+            locations.get(1).addExit("E", 3);
+            locations.get(1).addExit("S", 4);
+            locations.get(1).addExit("N", 5);
+
+            locations.get(2).addExit("N", 5);
+
+            locations.get(3).addExit("W", 1);
+
+            locations.get(4).addExit("N", 1);
+            locations.get(4).addExit("W", 2);
+
+            locations.get(5).addExit("S", 1);
+            locations.get(5).addExit("W", 2);
+
+            Map<String, String> vocabulary = new HashMap<String, String>();
+            vocabulary.put("QUIT", "Q");
+            vocabulary.put("NORTH", "N");
+            vocabulary.put("SOUTH", "S");
+            vocabulary.put("WEST", "W");
+            vocabulary.put("EAST", "E");
+
             int loc = 1;
             while(true) {
                 System.out.println(locations.get(loc).getDescription());
@@ -40,34 +60,31 @@ public class Main {
                     break;
                 }
 
-                loc = scanner.nextInt();
-                if(!locations.containsKey(loc)) {
+                Map<String, Integer> exits = locations.get(loc).getExits();
+                System.out.print("Available exits are ");
+                for(String exit: exits.keySet()) {
+                    System.out.print(exit + ", ");
+                }
+                System.out.println();
+
+                String direction = scanner.nextLine().toUpperCase();
+                if(direction.length() > 1) {
+                    String[] words = direction.split(" ");
+                    for(String word: words) {
+                        if(vocabulary.containsKey(word)) {
+                            direction = vocabulary.get(word);
+                            break;
+                        }
+                    }
+                }
+
+                if(exits.containsKey(direction)) {
+                    loc = exits.get(direction);
+
+                } else {
                     System.out.println("You cannot go in that direction");
                 }
             }
 
-
-            locations.get(1).addExit("W", 2);
-            locations.get(1).addExit("E", 3);
-            locations.get(1).addExit("S", 4);
-            locations.get(1).addExit("N", 5);
-//        locations.get(1).addExit("Q", 0);
-
-            locations.get(2).addExit("N", 5);
-//        locations.get(2).addExit("Q", 0);
-
-            locations.get(3).addExit("W", 1);
-//        locations.get(3).addExit("Q", 0);
-
-            locations.get(4).addExit("N", 1);
-            locations.get(4).addExit("W", 2);
-//        locations.get(4).addExit("Q", 0);
-
-            locations.get(5).addExit("S", 1);
-            locations.get(5).addExit("W", 2);
-//        locations.get(5).addExit("Q", 0);
-
         }
-
-//    }
 }
