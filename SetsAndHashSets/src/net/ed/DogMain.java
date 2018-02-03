d83acad7d2117b7e26fe836645869acf942ae0dd
Edward Dupre <edwd42@gmail.com>
2018-02-02 22:45:38 -0500

    add class SetMain

--- SetsAndHashSets/src/net/ed/DogMain.java
+++ SetsAndHashSets/src/net/ed/DogMain.java
@@ -1,7 +1,7 @@
 package net.ed;
 
 public class DogMain {
-    
+
     public static void main(String[] args) {
         Labrador rover = new Labrador("Rover");
         Dog rover2 = new Dog("Rover");
